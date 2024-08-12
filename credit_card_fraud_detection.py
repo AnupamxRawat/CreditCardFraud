f7f55e04b9f8af569f70e4b9ddeb4a8bb18dce1c
Anupam Rawat <132685598+AnupamxRawat@users.noreply.github.com>
2024-08-12 15:28:04 +0530

    credit_card_fraud_detection.py

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 
@@ -58,7 +58,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratif
 print(X.shape, X_train.shape, X_test.shape)
 
 
-model =MultinomialNB()
+model =LogisticRegression()
 
 model.fit(X_train, Y_train)
 
